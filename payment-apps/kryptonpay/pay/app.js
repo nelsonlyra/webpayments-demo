c5dc5c1bfd5d4e57c8ea30457bcb334dfd3162ba
Nelson Nunes <nelson.nunes@lyra-network.com>
2017-04-04 10:19:03 +0200

    commit

--- payment-apps/kryptonpay/pay/app.js
+++ payment-apps/kryptonpay/pay/app.js
@@ -1,43 +1,18 @@
-alert("NNU : Running service worker");
-
 self.addEventListener('paymentrequest', function(event) {
-    
-    console.log("NNU : PaymentRequest: " + JSON.stringify(event.data));
-
     event.respondWith(new Promise(function(resolve, reject) {
-        alert("NNU : SW paymentrequest "+event.data);
-
-        
-        //self.addEventListener('message', function(event) {
-            var response = event.data;
-            console.log("NNU : PaymentResponse: " + JSON.stringify(response));
-            
-            if (response) {
-                response.complete = function() {
-                    console.log("NNU : PaymentResponse.complete()");
-                    return Promise.resolve();
-                }
-                try {
-                    resolve(response);
-                } catch(error) {
-                    console.log("NNU :  "+error);
-                    reject(error);
-                }
-            } else {
-                reject();
+        var response = event.data;
+        if (response) {
+            response.complete = function() {
+                return Promise.resolve();
             }
-       // });
-
-        /*
-        clients.openWindow("index.html").then(function(windowClient) {
-            console.log("NNU : window opened!");
-            windowClient.postMessage(event.data);
-        })
-        .catch(function(error) {
-            console.log("NNU : Error "+error);
-            reject(error);
-        });
-        */
+            try {
+                resolve(response);
+            } catch(error) {
+                alert(error);
+                reject(error);
+            }
+        } else {
+            reject();
+        }
     }));
 });
-
