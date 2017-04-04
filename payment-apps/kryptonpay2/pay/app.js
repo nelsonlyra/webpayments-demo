9b490e9a7688b7ab5a9db898d38e1c5d389e3dae
Nelson Nunes <nelson.nunes@lyra-network.com>
2017-04-04 11:14:25 +0200

    commit

--- payment-apps/kryptonpay2/pay/app.js
+++ payment-apps/kryptonpay2/pay/app.js
@@ -1,23 +1,28 @@
 self.addEventListener('paymentrequest', function(event) {
     event.respondWith(new Promise(function(resolve, reject) {
-        var response = {
-            methodName: "https://kryptonpay.no/pay",
-            details: {
-                test: event.data
+        self.addEventListener('message', function(event) {
+            var response = event.data;
+            if (response) {
+                response.complete = function() {
+                    console.log("PaymentResponse.complete()");
+                    return Promise.resolve();
+                }
+                try {
+                    resolve(response);
+                } catch(error) {
+                    alert(error);
+                    reject(error);
+                }
+            } else {
+                reject();
             }
-        };
-        if (response) {
-            response.complete = function() {
-                return Promise.resolve();
-            }
-            try {
-                resolve(response);
-            } catch(error) {
-                alert(error);
-                reject(error);
-            }
-        } else {
-            reject();
-        }
+        });
+        clients.openWindow("index.html").then(function(windowClient) {
+            windowClient.postMessage(event.data);
+        })
+        .catch(function(error) {
+            reject(error);
+        });
     }));
 });
+
